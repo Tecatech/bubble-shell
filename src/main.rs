005bbb0a7950a91925c161507c823b7d5eb7755b
Dmitriy Rodionov <57829161+Tecatech@users.noreply.github.com>
2021-05-03 22:20:24 +0300

    resources added

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
             let args = parts;
             match command {
                 "cd" => {
-                    let new_dir = args.peekable().peek().map_or("/", |x| * x);
+                    let new_dir = args.peekable().peek().map_or("/", | x | * x);
                     let root = Path::new(new_dir);
                     if let Err(e) = env::set_current_dir(&root) {
                         eprintln!("{}", e);
